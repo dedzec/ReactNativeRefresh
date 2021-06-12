447d4f8fe1cf3150f918870a47ac38b791d878ca
dedzec <dedzec@gmail.com>
2021-06-12 15:19:11 -0300

    first commit

--- android/app/src/main/java/com/reactnativerefresh/MainApplication.java
+++ android/app/src/main/java/com/reactnativerefresh/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnative;
+package com.reactnativerefresh;
 
 import android.app.Application;
 import android.content.Context;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import {
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const wait = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
 
 const App = () => {
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>REACT NATIVE</Text>
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <Text>Pull down to see RefreshControl indicator</Text>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -12,6 +35,10 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
